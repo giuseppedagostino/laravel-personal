3f52674feea8890786fd4371f10495ecfca0e6df
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-25 11:23:35 +0100

    Creazione Destroy
    1 - Sistemazione layout
    2 - Creazione funzione destroy

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,17 +8,27 @@
   </div>
   @endif  
 
-  <div class="d-flex flex-wrap bg-secondary">
+  <div class="d-flex flex-wrap bg-secondary justify-content-center">
+    
     @foreach ($articles as $article)
       <div class="card m-3" style="width: 20rem;">
         <img class="card-img-top" src="{{ $article->image }}" alt="{{ $article->title }}">
         <div class="card-body">
           <h3 class="card-title">{{ $article->title }}</h3>
           <h5 class="card-title">{{ $article->subtitle }}</h5>
-          <p class="card-text">{{ substr($article->content, 0, 100) . " ..." }}</p>
+          <p class="card-text">{{ substr($article->content, 0, 50) . " ..." }}</p>
           <p class="card-text">{{ $article->author }} - {{ $article->publication_date }}</p>
-          {{-- DEVO PASSARE LO SLUG PERCHE' LO UTILIZZI --}}
+
+          {{-- SHOW --}}
           <a href="{{ route('articles.show', $article->slug) }}" class="btn btn-primary">Leggi di più</a>
+          {{-- DESTROY --}}
+
+          <form action="{{ route('articles.destroy', $article->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger">Elimina</button>
+          </form>
+
         </div>
       </div>
     @endforeach
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('main')
 
-  <div class="container">
+  <div class="container my-4">
     <h2 class="display-4 text-center">{{ $articles->title}}</h2>
     <h3 class="text-center mb-4">{{ $articles->subtitle}}</h3>
-    <img class="card-img-top" src="{{ $articles->image }}" alt="Card image cap" style="width: 500px">
+    <img class="card-img-top" src="{{ $articles->image }}" alt="{{ $articles->title}}" style="width: 500px">
     <hr>
     <p class="mt-4">Author: {{ $articles->content }}</p>
     <p><strong>Autore: </strong>{{ $articles->author }}</p>
